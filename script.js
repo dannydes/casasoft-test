b2137cec08f3511b831c3d0d09959dbc9f482bf9
Daniel <desiradaniel2007@hotmail.com>
2016-08-15 13:50:17 +0200

    slider buttons placement hack

--- script.js
+++ script.js
@@ -3,11 +3,30 @@
 		arrows: false
 	});
 	
+	var images = [
+		'a-cruise-ship-high-res-wallpaper.jpg',
+		'cargo-vessel.jpg'
+	];
+	
+	var counter = 0;
+	
+	changeSliderBackground();
+	
 	$( '#slider-prev' ).click(function () {
 		slider.unslider( 'prev' );
+		
+		counter = ( counter === 0 ? images.length - 1 : --counter );
+		changeSliderBackground();
 	});
 	
 	$( '#slider-next' ).click(function () {
 		slider.unslider( 'next' );
+		
+		counter = ( counter === images.length - 1 ? 0 : ++counter );
+		changeSliderBackground();
 	});
+	
+	function changeSliderBackground() {
+		$( '#slider-area' ).css( 'backgroundImage', 'url("' + images[counter] + '")' );
+	}
 })( jQuery );
